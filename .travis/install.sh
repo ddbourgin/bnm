a5d782afb34afc3ca941e3a7d843f1996d607c14
David Bourgin <ddbourgin@gmail.com>
2018-05-06 00:14:01 -0700

    install pip and pyenv manually for osx builds

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,17 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew cask install google-chrome
+    brew install openssl readline
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    pyenv install $PYTHON
+    export PYENV_VERSION=$PYTHON
+    export PATH="/Users/travis/.pyenv/shims:${PATH}"
+    pyenv-virtualenv venv
+    source venv/bin/activate
+
+    # A manual check that the correct version of Python is running.
+    python --version
 else
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
