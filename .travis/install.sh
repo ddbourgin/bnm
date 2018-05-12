9e9c5504b2d69f0089f5cc22e0f8c9a63dfe99f4
David Bourgin <ddbourgin@gmail.com>
2018-05-11 18:46:48 -0700

    activate venv before running tox tests on osx

--- .travis/install.sh
+++ .travis/install.sh
@@ -20,6 +20,6 @@ else
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
     sudo apt-get update
-    sudo apt-get install unzip google-chrome-stable xvfb
+    sudo apt-get install google-chrome-stable 
     pip install tox-travis
 fi
---
+++ .travis/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    export PYENV_VERSION=$PYTHON
+    export PATH="/Users/travis/.pyenv/shims:${PATH}"
+    pyenv virtualenv venv
+    source venv/bin/activate
+    tox
+else
+    tox
+fi
