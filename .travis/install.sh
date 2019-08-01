1a14f742bea9a395295347795e6e5d3c8e861822
David Bourgin <ddbourgin@users.noreply.github.com>
2019-08-01 02:01:09 -0400

    Remove chrome dependency

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@
 echo '[Running install.sh]'
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
-    brew cask install google-chrome
+#     brew cask install google-chrome
     brew install openssl readline
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
@@ -17,9 +17,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     echo $(python --version)
     pip install tox-travis
 else
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+#     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
     sudo apt-get update
-    sudo apt-get install google-chrome-stable 
+#     sudo apt-get install google-chrome-stable 
     pip install tox-travis
 fi
