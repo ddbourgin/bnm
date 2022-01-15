6f22e07be67b12d877a204a7a3fc3315946ea9c5
David Bourgin <dbourgin@spotify.com>
2022-01-14 20:21:08 -0500

    read from requirements.txt for install_requires

--- setup.py
+++ setup.py
@@ -9,20 +9,23 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    requirements = f.read().split("\n")
+
 setup(
     name="bestnewmusic",
     description="View music reviews and weekly radio charts in the terminal",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ddbourgin/bestnewmusic",
-    version="0.5.5",
+    version="0.5.6",
     author="David Bourgin",
     author_email="ddbourgin@gmail.com",
     license="MIT",
     keywords=["music", "terminal"],
     packages=["bestnewmusic"],
     entry_points={"console_scripts": ["bnm = bestnewmusic.__main__:main"]},
-    install_requires=["requests", "termcolor", "beautifulsoup4", "html5lib"],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
